9244587c299efcbfe445031f22dbb3facda82cd7
Miguel Julio <fitfab@yahoo.com>
2023-10-02 11:12:14 -0400

    styles: setup mobile nav

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,11 @@
 // NOTE: This is the index route the "root route"
 import type { MetaFunction } from "@remix-run/cloudflare";
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import { GraphQLClient, gql } from "graphql-request";
-import { useRef } from "react";
-import { Card, Logo } from "~/components";
+import { Menu, X } from "lucide-react";
+import { useRef, useState } from "react";
+import { Logo } from "~/components";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 
 const PAGE_QUERY = gql`
   query page($slug: String) {
@@ -52,48 +54,71 @@ export const loader = async () => {
   const variables = { slug: "visual-flight-technology" };
 
   const response = await graphQLClient.request(PAGE_QUERY, variables);
-  console.log("response:", response);
   return response;
 };
 
 export default function Index() {
   const { page } = useLoaderData<Response>();
   const serviceEl = useRef<HTMLDivElement | null>(null);
+  const contactEl = useRef<HTMLDivElement | null>(null);
+  const [mobileNavActive, setMobileNavActive] = useState(false);
 
-  const handleServiceClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    // event.preventDefault()
-    // 👇 Scroll to the last element in the list
-    serviceEl.current?.scrollIntoView();
+  const toggleNav = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(event.currentTarget.dataset.toggle);
+    setMobileNavActive(!mobileNavActive);
   };
+  const handleScroll = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const action = event.currentTarget.textContent?.toLowerCase();
+    // 👇 Scroll to the section
+    if (action === "services") {
+      serviceEl.current?.scrollIntoView();
+    } else {
+      contactEl.current?.scrollIntoView();
+    }
+  };
+  console.log("about to render");
   return (
+    // md:backdrop-blur-sm md:bg-slate-300/15
     <main>
-      <header className="fixed top-0 left-0 right-0 backdrop-blur-sm bg-slate-600/20 border-b-[1px] border-slate-300/15">
-        <div className="flex justify-between h-16 md:container md:mx-auto md:px-0 p-3">
-          <Logo />
-          <nav className="flex justify-center items-center gap-4  ">
+      <header className="fixed top-0 left-0 right-0  z-50">
+        <div className="relative flex justify-between items-center md:container md:mx-auto p-4">
+          <div className="w-24 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-white/25 from-0% to-transparent to-70%">
+            <Logo />
+          </div>
+          <button
+            className="fixed z-[9999] right-8 top-8"
+            aria-controls="primary-navigation"
+            data-toggle="open"
+            onClick={toggleNav}
+          >
+            <span className="sr-only">menu</span>
+            {mobileNavActive ? (
+              <X className="md:hidden" stroke="white" size={32} />
+            ) : (
+              <Menu className="md:hidden" stroke="white" size={32} />
+            )}
+          </button>
+          <nav
+            id="primary-navigation"
+            className="fixed inset-[0_0_0_32vw] bg-brand/50 backdrop-blur-md flex flex-col gap-4 pt-20 px-8 md:static md:flex-row md:pt-0 md:px-[1px] md:gap-[1px]"
+          >
             <button
-              onClick={handleServiceClick}
-              className="bg-white rounded-full block px-3 py-1 uppercase text-sm"
+              onClick={handleScroll}
+              className="block py-2 px-4 uppercase text-clamp-base cursor-pointer text-left text-white bg-white/20"
             >
               Services
             </button>
-            <Link
-              to="/About"
-              className="bg-white rounded-full block px-3 py-1 uppercase text-sm"
-            >
-              About
-            </Link>
-            <Link
-              to="/Contact"
-              className="bg-white rounded-full block px-3 py-1 uppercase text-sm"
+            <button
+              onClick={handleScroll}
+              className="block py-2 px-4 uppercase text-clamp-base cursor-pointer text-left text-white bg-white/20"
             >
               Contact
-            </Link>
+            </button>
           </nav>
         </div>
       </header>
       <video
-        className="w-full"
+        className="w-[100vw]"
         muted
         loop
         autoPlay
@@ -101,22 +126,52 @@ export default function Index() {
       >
         <source src="https://res.cloudinary.com/dcvxv60gw/video/upload/q_auto,f_auto/v1686666056/fitfab/neil_landino_ojaxke" />
       </video>
-      <section className="u-container">
-        <div className="block" ref={serviceEl}>
-          <h2>Serivices</h2>
-          <div className="columns">
+      <section className="md:container md:mx-auto px-4 pt-8 pb-10">
+        <div ref={serviceEl}>
+          <h2 className="scroll-m-20 pb-2 text-clamp-xl font-semibold tracking-tight transition-colors first:mt-0">
+            Serivices
+          </h2>
+          <div className="grid grid-cols-1 gap-4  md:grid-cols-3">
             {page.services.map(({ title, caption, callToAction }, index) => {
               return (
-                <Card
-                  key={index}
-                  title={title}
-                  caption={caption}
-                  ctaCopy={callToAction.ctaCopy}
-                  ctaUrl={callToAction.ctaUrl}
-                />
+                <Card key={index}>
+                  <CardHeader>
+                    <CardTitle>{title}</CardTitle>
+                  </CardHeader>
+
+                  <CardContent>{caption}</CardContent>
+                  {/* <CardFooter>
+                    <Link to={callToAction.ctaUrl}>{callToAction.ctaCopy}</Link>
+                  </CardFooter> */}
+                </Card>
               );
             })}
           </div>
+        </div>
+      </section>
+      <section className=" bg-brand text-white">
+        <div
+          className="md:container md:mx-auto px-4 pt-8 pb-10"
+          ref={contactEl}
+        >
+          <h2 className="scroll-m-20 pb-2 text-clamp-xl font-semibold tracking-tight transition-colors first:mt-0">
+            Contact Us
+          </h2>
+          <p className="text-clamp-base mb-6 w-[75%]">
+            I take the art of filmmaking to new heights with cutting-edge drone
+            technology. I value your engagement and am eager to assist you in
+            realizing your creative vision.
+          </p>
+          <p className="text-clamp-base mb-6">
+            Various ways to connect with me:
+          </p>
+          <p className="text-clamp-base">Andrew Pollino</p>
+          <p className="text-clamp-base">
+            <strong>Email</strong>: pollino146@gmail.com
+          </p>
+          <p className="text-clamp-base">
+            <strong>Phone</strong>: 203-957-2779
+          </p>
         </div>
       </section>
     </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,56 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      colors: {
+        brand: "#003049",
+      },
       fontFamily: {
         Mona: "Mona Sans",
       },
+      // REF: https://clamp.font-size.app/
+      fontSize: {
+        // sm: 12px - 14px
+        "clamp-sm": "clamp(0.75rem, 0.6071rem + 0.2976vw, 0.875rem)",
+        // base: 16px - 18px
+        "clamp-base": "clamp(1rem, 0.8571rem + 0.2976vw, 1.125rem)",
+        // lg: 20px - 24px
+        "clamp-lg": "clamp(1.25rem, 0.9643rem + 0.5952vw, 1.5rem)",
+        // xl: 28px - 32px
+        "clamp-xl": "clamp(1.75rem, 1.4643rem + 0.5952vw, 2rem)",
+        // xxl: 36px - 40px
+        "clamp-2xl": "clamp(2.25rem, 1.9643rem + 0.5952vw, 2.5rem)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
